ff0e5857284678ddda2503dd07cf9b15c4572d37
Rajneesh Tailor <rajneesh071@gmail.com>
2020-11-24 16:06:45 +0530

    Initial Commit

---
+++ AzurePipelineTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AzurePipelineTesting
+//
+//  Created by Rajneesh on 24/11/20.
+//  Copyright © 2020 BRRV. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AzurePipelineTestingTests/AzurePipelineTestingTests.swift
@@ -0,0 +1,33 @@
+//
+//  AzurePipelineTestingTests.swift
+//  AzurePipelineTestingTests
+//
+//  Created by Rajneesh on 24/11/20.
+//  Copyright © 2020 BRRV. All rights reserved.
+//
+
+import XCTest
+@testable import AzurePipelineTesting
+
+class AzurePipelineTestingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        XCTAssertTrue(1==1, "True")
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
